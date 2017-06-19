884ed4ddd6418032cd0daf6f4339b9947aa3e690
Brian Whitton <me@brianwhitton.com>
2017-06-18 22:59:10 -0400

    add routes to dummy app for testing

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
+import PreviousMixin from 'ember-previous/mixins/previous';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend(PreviousMixin, {
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('foo');
+  this.route('bar');
 });
 
 export default Router;
